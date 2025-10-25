2267cdc4031596d012acbb9f60b1e9bd804d0c09
mustafe xasan <mustafehaybe2003@gmail.com>
2025-10-25 08:38:45 +0300

    commited all

--- change.js
+++ change.js
@@ -1,3 +1,8 @@
 console.log("change 1")
 console.log("change 1")
 console.log("change 1")
+
+
+for(let i=0; i<=10; i+2){
+    console.log(i)
+}
--- config.js
+++ config.js
@@ -2,3 +2,9 @@ console.log("first")
 console.log("second 2")
 console.log("second 3")
 console.log("second 4")
+
+
+console.log("hada")
+
+
+
